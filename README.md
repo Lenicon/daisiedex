0381b5b64bfda5cf883441ac68e2dd35d015b7a9
Len.icon <lenthesmiledev@gmail.com>
2026-01-06 16:10:17 +0800

    plantlady_mobile

---
+++ README.md
@@ -0,0 +1,3 @@
+# plantlady_mobile
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/plantlady_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.plantlady_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
